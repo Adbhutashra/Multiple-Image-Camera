e58e01f47930ab44f99ef666c6c2c5c398015d8d
Simeon <gar17052@byui.edu>
2024-02-07 15:14:12 -0700

    Add widget variables for use in CameraFile

--- lib/multiple_image_camera.dart
+++ lib/multiple_image_camera.dart
@@ -6,7 +6,11 @@ import 'package:multiple_image_camera/camera_file.dart';
 class MultipleImageCamera {
   static Future<List<MediaModel>> capture({
     required BuildContext context,
-    Widget? customDoneButton
+    Widget? customDoneButton,
+    Widget? rotateCameraIcon,
+    ButtonStyle? backButtonStyle,
+    Icon? cancelIcon,
+    int? maxPictures,
   }) async {
     List<MediaModel> images = [];
     try {
@@ -15,6 +19,10 @@ class MultipleImageCamera {
           MaterialPageRoute(
               builder: (BuildContext context) =>  CameraFile(
                 customButton: customDoneButton,
+                rotateCameraIcon: rotateCameraIcon,
+                backButtonStyle: backButtonStyle,
+                cancelIcon: cancelIcon,
+                maxPictures: maxPictures,
               )));
     // ignore: empty_catches
     } catch (e) {
